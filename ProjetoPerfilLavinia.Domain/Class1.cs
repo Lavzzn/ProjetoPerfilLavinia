34abdcdfafec0f5d703a2d49be92a95b966d4f12
47802213827 <47802213827@INF0208>
2025-06-11 13:41:25 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoPerfilLavinia.Domain/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjetoPerfilLavinia.Domain
+{
+    public class Class1
+    {
+
+    }
+    public class Animal
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Especie { get; set; }
+    }
+}
